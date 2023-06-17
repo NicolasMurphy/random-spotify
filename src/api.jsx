41d6a4e1d5cfbc17f1ca7b750391ab706df249c1
Nicolas Murphy <murphynick10@gmail.com>
2023-06-17 12:50:57 -0500

    only returns tracks with preview url that is available

--- src/api.jsx
+++ src/api.jsx
@@ -28,30 +28,38 @@ export const refreshAccessToken = async (setAccessToken) => {
   export const getRandomTrack = async (accessToken, setTrack, setIsLoading, resetAudio) => {
     try {
       setIsLoading(true);
+      let randomTrack = null;
 
-      const searchTerm = await fetch('https://random-word-api.vercel.app/api?words=1')
-        .then((response) => response.json())
-        .then((data) => data[0]);
+      while (!randomTrack || !randomTrack.preview_url) {
+        const searchTerm = await fetch('https://random-word-api.vercel.app/api?words=1')
+          .then((response) => response.json())
+          .then((data) => data[0]);
 
-      const response = await fetch(
-        `https://api.spotify.com/v1/search?q=${encodeURIComponent(searchTerm)}&type=track`,
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
+        const response = await fetch(
+          `https://api.spotify.com/v1/search?q=${encodeURIComponent(searchTerm)}&type=track`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`,
+            },
+          }
+        );
+
+        if (response.ok) {
+          const data = await response.json();
+          const tracks = data.tracks.items;
+
+          if (tracks.length > 0) {
+            randomTrack = tracks[0];
+          }
+        } else {
+          console.error('Error retrieving random track:', response.status);
+          setIsLoading(false);
+          return;
         }
-      );
-
-      if (response.ok) {
-        const data = await response.json();
-        const randomTrack = data.tracks.items[0];
-
-        setTrack(randomTrack);
-        resetAudio();
-      } else {
-        console.error('Error retrieving random track:', response.status);
       }
 
+      setTrack(randomTrack);
+      resetAudio();
       setIsLoading(false);
     } catch (error) {
       console.error('Error retrieving random track:', error);
